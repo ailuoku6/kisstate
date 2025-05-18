321d9cd4842dbb08ac9deabcd7ed316b950e54ff
ailuoku6 <ailuoku6@ailuoku6s-MacBook-Air.local>
2025-05-18 14:19:51 +0800

    feat: readme

--- packages/kiss-state/dist/decorators/index.d.ts
+++ packages/kiss-state/dist/decorators/index.d.ts
@@ -1,5 +1,5 @@
-export declare function ObservableClass<T extends new (...args: any[]) => object>(Constructor_: T): T;
 type PropertyKeyOf<T> = keyof T & string;
+export declare function ObservableClass<T extends new (...args: any[]) => object>(Constructor_: T): T;
 export declare function watchProps<T extends object>(...props: PropertyKeyOf<T>[]): <C extends T>(target: C, methodName: string, descriptor: PropertyDescriptor) => void;
 export declare function computed<T extends object>(...props: PropertyKeyOf<T>[]): <C extends T>(target: C, methodName: string, descriptor: PropertyDescriptor) => void;
 export {};
